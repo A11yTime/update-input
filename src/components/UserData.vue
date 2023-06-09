77d752d7327a3cb91723031467b75d73a02e2e35
Md Sohel Rana <m.sohel.sw@gmail.com>
2023-06-09 15:06:54 -0700

    updae import

---
+++ src/components/UserData.vue
@@ -0,0 +1,31 @@
+<template>
+<section>
+    <form @submit.prevent="submitData">
+    <label for="name">Name </label>
+    <input type="text" id="name" v-model="enteredName">
+    <label for="age">Age </label>
+    <input type="text" id="age" v-model="enteredAge">
+    <button>Set User Data</button>
+</form>
+</section>
+<div>
+    
+</div>
+</template>
+
+<script>
+export default{
+    emits: ['set-data'],
+ data(){
+    return{
+        enteredName: '',
+        enteredAge: ''
+    }
+ },
+ methods: {
+    submitData(){
+        this.$emit('set-data', this.enteredName, this.enteredAge)
+    }
+ }
+}
+</script>
